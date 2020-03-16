8047ca6d9978b9d4185065333fb5a873dce5a012
Val <kalenikvalentin@yahoo.com>
2020-03-16 01:41:09 -0500

    Rename DataTests to DataServiceTests

--- TBD_Main_Branch/src/test/java/DataServiceTests.java
+++ TBD_Main_Branch/src/test/java/DataServiceTests.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DataTests {
+public class DataServiceTests {
 
     private DataService dataService;
     private String directoryPath;
